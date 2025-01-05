d633e964768f8e095e29b64fddef914992ded9f2
Jason Ericson <jason.l.ericson@gmail.com>
2025-01-05 15:42:58 -0600

    Ch4 - Defining and Using Functions - Using Default and Rest Parameters

--- src/example.js
+++ src/example.js
@@ -1,6 +1,16 @@
-function myFunc(name, weather) {
+/* function myFunc(name, weather = "raining") {
   console.log("Hello " + name + ".");
   console.log("It is " + weather + " today.");
 }
 
-myFunc("Adam", "sunny");
+myFunc("Adam"); */
+
+function myFunc(name, weather, ...extraArgs) {
+  console.log("Hello " + name + ".");
+  console.log("It is " + weather + " today.");
+  for (let i = 0; i < extraArgs.length; i++) {
+    console.log("Extra Arg: " + extraArgs[i]);
+  }
+}
+
+myFunc("Adam", "sunny", "one", "two", "three");
