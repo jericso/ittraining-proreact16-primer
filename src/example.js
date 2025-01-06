abd41bb788c6dccf2a2a3f390552d2eb0cc83854
Jason Ericson <jason.l.ericson@gmail.com>
2025-01-06 14:50:24 -0600

    Ch4 - Using Variables and Types - Working with Strings - Using Template Strings

--- src/example.js
+++ src/example.js
@@ -1,26 +1,6 @@
-/* function messageFunction(name, weather) {
-  // let message = "Hello, Adam";
-  var message = "Hello, Adam";
-  if (weather === "sunny") {
-    // let message = "It is a nice day";
-    var message = "It is a nice day";
-    console.log(message);
-  } else {
-    // let message = "It is " + weather + " today";
-    var message = "It is " + weather + " today";
-    console.log(message);
-  }
+function messageFunction(weather) {
+  let message = `It is ${weather} today`;
   console.log(message);
 }
 
-messageFunction("Adam", "raining"); */
-
-function myFunc(name) {
-  let myLocalVar = "sunny";
-  let innerFunction = function () {
-    return "Hello " + name + ". Today is " + myLocalVar + ".";
-  };
-  return innerFunction();
-}
-
-console.log(myFunc("Adam"));
+messageFunction("raining");
