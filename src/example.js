b15d1ed2c3e791aa0a8b58b0d39d2d31c4d9fc2b
Jason Ericson <jason.l.ericson@gmail.com>
2025-01-06 14:44:31 -0600

    Ch4 - Using Variables and Types

--- src/example.js
+++ src/example.js
@@ -1,8 +1,26 @@
-const myFunc = (nameFunction) => "Hello " + nameFunction() + ".";
+/* function messageFunction(name, weather) {
+  // let message = "Hello, Adam";
+  var message = "Hello, Adam";
+  if (weather === "sunny") {
+    // let message = "It is a nice day";
+    var message = "It is a nice day";
+    console.log(message);
+  } else {
+    // let message = "It is " + weather + " today";
+    var message = "It is " + weather + " today";
+    console.log(message);
+  }
+  console.log(message);
+}
 
-const printName = (nameFunction, printFunction) =>
-  printFunction(myFunc(nameFunction));
+messageFunction("Adam", "raining"); */
 
-printName(function () {
-  return "Adam";
-}, console.log);
+function myFunc(name) {
+  let myLocalVar = "sunny";
+  let innerFunction = function () {
+    return "Hello " + name + ". Today is " + myLocalVar + ".";
+  };
+  return innerFunction();
+}
+
+console.log(myFunc("Adam"));
