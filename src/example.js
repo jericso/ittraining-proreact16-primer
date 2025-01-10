24dc03bf8822b9c743aa030d51ae7e0a6b7f5b13
Jason Ericson <jason.l.ericson@gmail.com>
2025-01-10 13:19:55 -0600

    Using JavaScript Operators - The Equality Operator vs. the Identity Operator

--- src/example.js
+++ src/example.js
@@ -1,21 +1,10 @@
-let name = "Adam";
+let firstVal = 5;
+let secondVal = "5";
 
-if (name === "Adam") {
-  console.log("Name is Adam");
-} else if (name === "Jacqui") {
-  console.log("Name is Jacqui");
+// equality operator: if (firstVal == secondVal) {
+// identity operator
+if (firstVal === secondVal) {
+  console.log("They are the same");
 } else {
-  console.log("Name is neither Adam or Jacqui");
-}
-
-switch (name) {
-  case "Adam":
-    console.log("Name is Adam");
-    break;
-  case "Jacqui":
-    console.log("Name is Jacqui");
-    break;
-  default:
-    console.log("Name is neither Adam or Jacqui");
-    break;
+  console.log("They are NOT the same");
 }
